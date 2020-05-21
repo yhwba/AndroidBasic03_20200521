5cef6462d80dd7820458c49f67d65185d3322b7e
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 17:49:59 +0900

    입력된 전화번호에 연결하도록 수정

--- app/src/main/java/kr/co/yhw/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic03_20200521/MainActivity.java
@@ -32,7 +32,9 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 //                전화걸기  안드로이드 =Uri정보 요구
-                Uri myUri = Uri.parse("tel:010-0000-0000");
+                String dialPhoneNum =binding.userPhoneNum.getText().toString();
+
+                Uri myUri = Uri.parse(String.format("tel : %s",dialPhoneNum));
                 Intent myIntent = new Intent(Intent.ACTION_DIAL,myUri);
                 startActivity(myIntent);
             }
