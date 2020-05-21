2dc2df35ac2230fcaaee85f27c2cd04425318b9c
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 16:07:10 +0900

    전화번호 수정화면 그리기

---
+++ app/src/main/java/kr/co/yhw/androidbasic03_20200521/EditUserPhoneNumActivity.java
@@ -0,0 +1,27 @@
+package kr.co.yhw.androidbasic03_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserPhoneNumActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_phone_num);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
