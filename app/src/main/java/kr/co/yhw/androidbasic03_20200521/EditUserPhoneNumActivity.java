5f9e14e6c245a526740d671299b5cefb84e5a9be
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 16:40:52 +0900

    전화번호 넣고 돌아갈 데이터 셋팅, 액티비티 종료

--- app/src/main/java/kr/co/yhw/androidbasic03_20200521/EditUserPhoneNumActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic03_20200521/EditUserPhoneNumActivity.java
@@ -1,23 +1,41 @@
 package kr.co.yhw.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import kr.co.yhw.androidbasic03_20200521.databinding.ActivityEditUserPhoneNumBinding;
 
 public class EditUserPhoneNumActivity extends BaseActivity {
 
+    ActivityEditUserPhoneNumBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_phone_num);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_user_phone_num);
         setupEvents();
         setValues();
     }
 
     @Override
     public void setupEvents() {
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPhoneNum = binding.EdtPhoneNum.getText().toString();
 
+                Intent intent = new Intent();
+                intent.putExtra("phone",inputPhoneNum);
+
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+
+        });
     }
 
     @Override
