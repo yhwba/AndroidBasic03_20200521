845fd5d12fcf90f79dcfd2003087dbe1d685f6e8
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 15:19:12 +0900

    닉네임 수정화면 추가 - 그림을 따로 그려야함

---
+++ app/src/main/java/kr/co/yhw/androidbasic03_20200521/EditUserNickNameActivity.java
@@ -0,0 +1,26 @@
+package kr.co.yhw.androidbasic03_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserNickNameActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_nick_name);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
