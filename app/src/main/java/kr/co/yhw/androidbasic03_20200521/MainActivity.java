5b443bc714a9b3210713512d15ae8a6db0820133
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 16:21:59 +0900

    폰번 수정화면 호출 - startActivityForResult -어떤 요청인지를 멤버변수에 담아서 구별

--- app/src/main/java/kr/co/yhw/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic03_20200521/MainActivity.java
@@ -1,18 +1,21 @@
 package kr.co.yhw.androidbasic03_20200521;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
 
 import kr.co.yhw.androidbasic03_20200521.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding binding;
+
+    final int REQ_FOR_PHONE_NUM =1002;
+    final int REQ_FOR_NICK_NAME =1001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +26,20 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.EdtPhoneNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext,EditUserPhoneNumActivity.class);
+                startActivityForResult(myIntent, REQ_FOR_PHONE_NUM);
+
+            }
+        });
         View.OnClickListener ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(mContext, EditUserNickNameActivity.class);
-                startActivityForResult(myIntent, 1001);
+                startActivityForResult(myIntent, REQ_FOR_NICK_NAME);
             }
         };
 
@@ -42,7 +54,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1001){
+        if(requestCode == REQ_FOR_NICK_NAME){
             if (resultCode == RESULT_OK){
 
                 String nick = data.getStringExtra("editedNickName");
